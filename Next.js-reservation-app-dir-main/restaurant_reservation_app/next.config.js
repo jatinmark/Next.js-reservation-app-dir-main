b9b2290be2d351724a90eeb8a2a200b630c27bd2
jatin choudhary <73272963+jatinmark@users.noreply.github.com>
2023-11-04 21:02:04 +0530

    Update next.config.js

--- Next.js-reservation-app-dir-main/restaurant_reservation_app/next.config.js
+++ Next.js-reservation-app-dir-main/restaurant_reservation_app/next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     async rewrites() {
       return [
         {
-          source: 'http://localhost:3000/api/auth/signin',
+          source: '/api/auth/signin',
           destination: 'https://reservation-app-virid.vercel.app',
         },
       ]
